d66947738708cebc73f6e3a109b8fe81cad46013
frank-Nhatvm <nhat.thtb@gmail.com>
2025-10-25 15:50:05 +0700

    add all players

--- composeApp/src/webMain/kotlin/com/fatherofapps/fc671/ListPlayers.kt
+++ composeApp/src/webMain/kotlin/com/fatherofapps/fc671/ListPlayers.kt
@@ -1,8 +1,14 @@
 package com.fatherofapps.fc671
 
 val allPlayers = listOf(
-    "Chu tich", "Anh Vu", "Tung Tit", "Phuong To", "Hieu teva",
-    "Do Tran", "Thanh An", "Son Xoan", "Vinh", "Nghia Bia", "Tung HD",
-    "Tien Beo","Manh Map", "Nhat Mitoma","Nam chelsea","Thoa Loa","Thanh TS",
-    "Huy Phung", "Tuan rau"
+    "Chu tich", "Anh Vũ", "Tùng Tít", "Sơn Xoán",
+    "Thành TS", "Tùng Chè","Tuấn Râu",
+    "Phương Tổ", "Ba Duy",  "Hiếu Teva",
+    "Kanu", "Trung CAX", "Kiên", "Lê An", "Thuận",
+    "Mạnh Mập", "Nam Chelsea", "Nghĩa Bia",
+    "Sâm Nhung","Thành An", "Thắng", "Tiến ĐC",
+    "Trần Vũ An", "Tuyên Lê", "Việt Leo",
+    "Quân ĐC", "Thắng Thanh Hoà", "Vinh", "Huy Phùng",
+    "Thoả", "Độ Trần", "Tùng HĐ", "Nhật Mitoma", "Lộc Hair",
+    "Duy B", "Tuấn BIDV", "Binh", "Bộ", "Fendi", "Trí Dũng"
 )
